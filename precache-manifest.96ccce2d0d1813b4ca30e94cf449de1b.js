39d468361a6fc2528fecbaf21a75d70995decf8c
emekdahl <emily@otus.com>
2019-10-13 12:04:22 -0500

    Updates

---
+++ precache-manifest.96ccce2d0d1813b4ca30e94cf449de1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9cdbf3072452b13e7d310cbe1978c1",
+    "url": "/data-engineering-projects/index.html"
+  },
+  {
+    "revision": "133d63ef1bd5b880dd0f",
+    "url": "/data-engineering-projects/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5e194f37f710b0465177",
+    "url": "/data-engineering-projects/static/js/2.1d95a5c5.chunk.js"
+  },
+  {
+    "revision": "133d63ef1bd5b880dd0f",
+    "url": "/data-engineering-projects/static/js/main.ef7952f6.chunk.js"
+  },
+  {
+    "revision": "4b348b893d1e51c1033f",
+    "url": "/data-engineering-projects/static/js/runtime-main.5499f12a.js"
+  }
+]);
